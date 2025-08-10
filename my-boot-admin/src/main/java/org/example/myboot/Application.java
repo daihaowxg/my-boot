3f018ddf0f464c557ecb6aa51706c82062a9d38f
daihaowxg <150903554+daihaowxg@users.noreply.github.com>
2025-08-10 23:13:15 +0800

    add application runner to display startup information

--- my-boot-admin/src/main/java/org/example/myboot/Application.java
+++ my-boot-admin/src/main/java/org/example/myboot/Application.java
@@ -1,14 +1,35 @@
 package org.example.myboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
 
-        System.out.println("Application started successfully!");
+    @Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            String appName = environment.getProperty("spring.application.name") != null ? environment.getProperty("spring.application.name") : "";
+            String port = environment.getProperty("server.port") != null ? environment.getProperty("server.port") : "8080";
+            String path = environment.getProperty("server.servlet.context-path") != null ? environment.getProperty("server.servlet.context-path") : "";
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            System.out.println(
+                    "\n\n\t" +
+                            "----------------------------------------------------------\n\t" +
+                            "Application " + appName +" is running! Access URLs:\n\t" +
+                            "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                            "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                            "------------------------------------------------------------" + "\n"
+            );
+        };
     }
 }
