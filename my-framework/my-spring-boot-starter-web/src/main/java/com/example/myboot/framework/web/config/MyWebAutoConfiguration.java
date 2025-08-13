51abce0188524f92f114e08ba5ffef3018db0d4e
daihaowxg <150903554+daihaowxg@users.noreply.github.com>
2025-08-13 23:24:44 +0800

    ✨ feat(web): 添加自定义 Web 配置和 API 属性
    - 新增 MyWebAutoConfiguration 类，配置 API 前缀
    - 新增 MyWebProperties 类，定义 API 和 UI 配置
    - 更新 pom.xml，添加 knife4j 和 springdoc 依赖

---
+++ my-framework/my-spring-boot-starter-web/src/main/java/com/example/myboot/framework/web/config/MyWebAutoConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.myboot.framework.web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@AutoConfiguration
+@EnableConfigurationProperties(MyWebProperties.class)
+public class MyWebAutoConfiguration implements WebMvcConfigurer {
+
+    @Resource
+    private MyWebProperties webProperties;
+
+    /**
+     * 应用名
+     */
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurePathMatch(configurer, webProperties.getAdminApi());
+        configurePathMatch(configurer, webProperties.getAppApi());
+    }
+
+    /**
+     * 设置 API 前缀，仅仅匹配 **.controller.admin.** 或 **.controller.app.** 包下的
+     *
+     * @param configurer 配置
+     * @param api        API 配置
+     */
+    private void configurePathMatch(PathMatchConfigurer configurer, MyWebProperties.Api api) {
+        // 默认 AntPathMatcher 使用 /，所以必须指定 . 才能正确匹配 Java 包名
+        AntPathMatcher antPathMatcher = new AntPathMatcher(".");
+        configurer.addPathPrefix(api.getPrefix(), clazz -> clazz.isAnnotationPresent(RestController.class)
+                && antPathMatcher.match(api.getController(), clazz.getPackage().getName())); // 仅仅匹配 controller 包
+    }
+}
