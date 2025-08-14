c812d025aa1c2354a7c6515860a5b6c1245d655d
daihaowxg <daihaowxg@gmail.com>
2025-08-14 12:31:55 +0800

    feat(api): 添加 API 前缀配置支持
    - 统一设置 admin 和 app API 的前缀
    - 修改 ProjectWebProperties 中的 API 配置顺序
    - 更新 configurePathMatch 方法以支持新的包名匹配规则

--- project-framework/project-spring-boot-starter-web/src/main/java/com/example/project/framework/web/config/ProjectWebAutoConfiguration.java
+++ project-framework/project-spring-boot-starter-web/src/main/java/com/example/project/framework/web/config/ProjectWebAutoConfiguration.java
@@ -24,6 +24,10 @@ public class ProjectWebAutoConfiguration implements WebMvcConfigurer {
     private String applicationName;
 
 
+    /**
+     * <p>{@code **.controller.admin.**} 包下有 {@code @RestController} 注解的 {@code Controller}，统一添加 {@code /admin-api} 前缀
+     * <p>{@code **.controller.app.**} 包下有 {@code @RestController} 注解的 {@code Controller}，统一添加 {@code /app-api} 前缀
+     */
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurePathMatch(configurer, webProperties.getAdminApi());
@@ -31,15 +35,19 @@ public class ProjectWebAutoConfiguration implements WebMvcConfigurer {
     }
 
     /**
-     * 设置 API 前缀，仅仅匹配 **.controller.admin.** 或 **.controller.app.** 包下的
+     * 包名符合 {@code packagePattern} 的 RestController 会被添加前缀 {@code prefix}
      *
      * @param configurer 配置
      * @param api        API 配置
+     * @see ProjectWebProperties.Api
      */
     private void configurePathMatch(PathMatchConfigurer configurer, ProjectWebProperties.Api api) {
         // 默认 AntPathMatcher 使用 /，所以必须指定 . 才能正确匹配 Java 包名
         AntPathMatcher antPathMatcher = new AntPathMatcher(".");
-        configurer.addPathPrefix(api.getPrefix(), clazz -> clazz.isAnnotationPresent(RestController.class)
-                && antPathMatcher.match(api.getController(), clazz.getPackage().getName())); // 仅仅匹配 controller 包
+        //
+        configurer.addPathPrefix(api.getPrefix(), // 要添加的前缀
+                clazz -> antPathMatcher.match(api.getPackagePattern(), clazz.getPackage().getName()) // 包名符合 packagePattern 要求
+                        && clazz.isAnnotationPresent(RestController.class) // 类上有 @RestController 注解
+        );
     }
 }
--- project-framework/project-spring-boot-starter-web/src/main/java/com/example/project/framework/web/config/ProjectWebProperties.java
+++ project-framework/project-spring-boot-starter-web/src/main/java/com/example/project/framework/web/config/ProjectWebProperties.java
@@ -16,11 +16,11 @@ import javax.validation.constraints.NotNull;
 @Data
 public class ProjectWebProperties {
 
-    @NotNull(message = "APP API 不能为空")
-    private Api appApi = new Api("/app-api", "**.controller.app.**");
-
     @NotNull(message = "Admin API 不能为空")
-    private Api adminApi = new Api("/admin-api", "**.controller.admin.**");
+    private Api adminApi = new Api("**.controller.admin.**", "/admin-api");
+
+    @NotNull(message = "APP API 不能为空")
+    private Api appApi = new Api("**.controller.app.**", "/app-api");
 
     @NotNull(message = "Admin UI 不能为空")
     private Ui adminUi;
@@ -32,24 +32,24 @@ public class ProjectWebProperties {
     public static class Api {
 
         /**
-         * API 前缀，实现所有 Controller 提供的 RESTFul API 的统一前缀
+         * Controller 所在包的 Ant 路径规则
          * <p>
-         *
+         * 主要目的是，给该包下的 {@code Controller} 设置指定的 {@link #prefix}
+         */
+        @NotEmpty(message = "Controller 所在包不能为空")
+        private String packagePattern;
+
+        /**
+         * 所有的 RESTFul API 提供统一的 API 前缀
+         * <p>
          * 意义：通过该前缀，避免 Swagger、Actuator 意外通过 Nginx 暴露出来给外部，带来安全性问题
-         *      这样，Nginx 只需要配置转发到 /api/* 的所有接口即可。
+         * <p>
+         * 这样，Nginx 只需要配置转发到 /api/* 的所有接口即可。
          *
          * @see ProjectWebAutoConfiguration#configurePathMatch(PathMatchConfigurer)
          */
         @NotEmpty(message = "API 前缀不能为空")
         private String prefix;
-
-        /**
-         * Controller 所在包的 Ant 路径规则
-         * <p>
-         * 主要目的是，给该 Controller 设置指定的 {@link #prefix}
-         */
-        @NotEmpty(message = "Controller 所在包不能为空")
-        private String controller;
 
     }
 
